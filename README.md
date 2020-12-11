f4f82f7b8ca0b62388cc94390f05f9e4217693db
mageswaran1989 <mageswaran1989@gmail.com>
2020-12-11 09:15:20 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # rl_flappy_bird
-Helping flappy bird to fly with Reinforcement Leanring
+Helping flappy bird to fly with Reinforcement Learning
+
+## Python Environment Setup
+```
+conda create -n flappy_bird
+pip install -r requirements.txt
+``` 
+
+## Run steps
+
+1. Flappy Bird Python version
+
+2. Train
+```
+python fb/dqn.py train
+```
+
+3. Test
+```
+python fb/dqn.py test
+```
+### References
+- Theory: [https://www.toptal.com/deep-learning/pytorch-reinforcement-learning-tutorial](https://www.toptal.com/deep-learning/pytorch-reinforcement-learning-tutorial)
+- Python Flappy Bird port : [https://github.com/sourabhv/FlapPyBird](https://github.com/sourabhv/FlapPyBird)
+- DQN: [https://github.com/nevenp/dqn_flappy_bird](https://github.com/nevenp/dqn_flappy_bird)
+- Convolutional Network
+    - Standford [CS231N](http://cs231n.stanford.edu/index.html)
+    - [https://stanford.edu/~shervine/teaching/cs-230/cheatsheet-convolutional-neural-networks](https://stanford.edu/~shervine/teaching/cs-230/cheatsheet-convolutional-neural-networks)
+- Paper : [https://storage.googleapis.com/deepmind-media/dqn/DQNNaturePaper.pdf](https://storage.googleapis.com/deepmind-media/dqn/DQNNaturePaper.pdf)
---
+++ fb/dqn.py
@@ -0,0 +1,211 @@
+import os
+import random
+import sys
+import time
+
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from fb.env.flappy_bird import GameState
+from fb.nn.conv_net import NeuralNetwork
+from fb.utils import resize_and_bgr2gray, image_to_tensor, init_weights
+
+
+def train(model, start, iteration=0):
+    # define Adam optimizer
+    optimizer = optim.Adam(model.parameters(), lr=1e-6)
+
+    # initialize mean squared error loss
+    criterion = nn.MSELoss()
+
+    # instantiate game
+    game_state = GameState(FPS=30)
+
+    # initialize replay memory
+    replay_memory = []
+
+    # initial action is do nothing
+    action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+    action[0] = 1
+    image_data, reward, terminal = game_state.frame_step(action)
+    image_data = resize_and_bgr2gray(image_data)
+    image_data = image_to_tensor(image_data)
+    state = torch.cat((image_data, image_data, image_data, image_data)).unsqueeze(0)
+
+    # initialize epsilon value
+    epsilon = model.initial_epsilon
+
+    epsilon_decrements = np.linspace(model.initial_epsilon, model.final_epsilon, model.number_of_iterations)
+
+    # main infinite loop
+    while iteration < model.number_of_iterations:
+        # get output from the neural network
+        output = model(state)[0]
+
+        # initialize action
+        action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            action = action.cuda()
+
+        # epsilon greedy exploration
+        random_action = random.random() <= epsilon
+        if random_action:
+            print("Performed random action!")
+        action_index = [torch.randint(model.number_of_actions, torch.Size([]), dtype=torch.int)
+                        if random_action
+                        else torch.argmax(output)][0]
+
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            action_index = action_index.cuda()
+
+        action[action_index] = 1
+
+        # get next state and reward
+        image_data_1, reward, terminal = game_state.frame_step(action)
+        image_data_1 = resize_and_bgr2gray(image_data_1)
+        image_data_1 = image_to_tensor(image_data_1)
+        state_1 = torch.cat((state.squeeze(0)[1:, :, :], image_data_1)).unsqueeze(0)
+
+        action = action.unsqueeze(0)
+        reward = torch.from_numpy(np.array([reward], dtype=np.float32)).unsqueeze(0)
+
+        # save transition to replay memory
+        replay_memory.append((state, action, reward, state_1, terminal))
+
+        # if replay memory is full, remove the oldest transition
+        if len(replay_memory) > model.replay_memory_size:
+            replay_memory.pop(0)
+
+        # epsilon annealing
+        epsilon = epsilon_decrements[iteration]
+
+        # sample random minibatch
+        minibatch = random.sample(replay_memory, min(len(replay_memory), model.minibatch_size))
+
+        # unpack minibatch
+        state_batch = torch.cat(tuple(d[0] for d in minibatch))
+        action_batch = torch.cat(tuple(d[1] for d in minibatch))
+        reward_batch = torch.cat(tuple(d[2] for d in minibatch))
+        state_1_batch = torch.cat(tuple(d[3] for d in minibatch))
+
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            state_batch = state_batch.cuda()
+            action_batch = action_batch.cuda()
+            reward_batch = reward_batch.cuda()
+            state_1_batch = state_1_batch.cuda()
+
+        # get output for the next state
+        output_1_batch = model(state_1_batch)
+
+        # set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q)
+        y_batch = torch.cat(tuple(reward_batch[i] if minibatch[i][4]
+                                  else reward_batch[i] + model.gamma * torch.max(output_1_batch[i])
+                                  for i in range(len(minibatch))))
+
+        # extract Q-value
+        q_value = torch.sum(model(state_batch) * action_batch, dim=1)
+
+        # PyTorch accumulates gradients by default, so they need to be reset in each pass
+        optimizer.zero_grad()
+
+        # returns a new Tensor, detached from the current graph, the result will never require gradient
+        y_batch = y_batch.detach()
+
+        # calculate loss
+        loss = criterion(q_value, y_batch)
+
+        # do backward pass
+        loss.backward()
+        optimizer.step()
+
+        # set state to be state_1
+        state = state_1
+        iteration += 1
+
+        if iteration % 25000 == 0:
+            torch.save(model, "pretrained_model/current_model_" + str(iteration) + ".pth")
+            torch.save(model, "pretrained_model/latest_model.pth")
+
+        print("iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon, "action:",
+              action_index.cpu().detach().numpy(), "reward:", reward.numpy()[0][0], "Q max:",
+              np.max(output.cpu().detach().numpy()))
+
+
+def test(model):
+    game_state = GameState()
+
+    # initial action is do nothing
+    action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+    action[0] = 1
+    image_data, reward, terminal = game_state.frame_step(action)
+    image_data = resize_and_bgr2gray(image_data)
+    image_data = image_to_tensor(image_data)
+    state = torch.cat((image_data, image_data, image_data, image_data)).unsqueeze(0)
+
+    while True:
+        # get output from the neural network
+        output = model(state)[0]
+
+        action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            action = action.cuda()
+
+        # get action
+        action_index = torch.argmax(output)
+        if torch.cuda.is_available():  # put on GPU if CUDA is available
+            action_index = action_index.cuda()
+        action[action_index] = 1
+
+        # get next state
+        image_data_1, reward, terminal = game_state.frame_step(action)
+        image_data_1 = resize_and_bgr2gray(image_data_1)
+        image_data_1 = image_to_tensor(image_data_1)
+        state_1 = torch.cat((state.squeeze(0)[1:, :, :], image_data_1)).unsqueeze(0)
+
+        # set state to be state_1
+        state = state_1
+
+
+def main(mode):
+    cuda_is_available = torch.cuda.is_available()
+
+    if mode == 'test':
+        model = torch.load(
+            'pretrained_model/latest_model.pth',
+            map_location='cpu' if not cuda_is_available else None
+        ).eval()
+
+        if cuda_is_available:  # put on GPU if CUDA is available
+            model = model.cuda()
+
+        test(model)
+
+    elif mode == 'train':
+        if not os.path.exists('pretrained_model/'):
+            os.mkdir('pretrained_model/')
+            model = NeuralNetwork()
+            max_index = 0
+        else:
+            model = torch.load(
+                'pretrained_model/latest_model.pth',
+                map_location='cpu' if not cuda_is_available else None
+            ).eval()
+            model_files = os.listdir('pretrained_model/')
+            model_files = [f for f in model_files if f.startswith("current_model_")]
+            indexes = [int(index.replace("current_model_", "").replace(".pth", "")) for index in model_files]
+            max_index = max(indexes)
+
+        if cuda_is_available:  # put on GPU if CUDA is available
+            model = model.cuda()
+
+        model.apply(init_weights)
+        start = time.time()
+
+        train(model, start, iteration=max_index)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
